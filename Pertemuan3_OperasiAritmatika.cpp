9bbf2bc47d03eec6ea81ce858fdd3e7cbdcfaf2f
muhammadrizkylpg17-code <muhammadrizkylpg17@gmail.com>
2025-12-19 21:18:05 +0700

    Add basic arithmetic operations in C++

---
+++ Pertemuan3_OperasiAritmatika.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 2;
+    int b = 3;
+    int c = a + b;
+
+    cout << "Nilai a : " << a << endl;
+    cout << "Nilai b : " << b << endl;
+    cout << "Nilai c : " << c << endl;
+
+    a = a + b;
+
+    cout << "Nilai a setelah (a = a + b) : " << a << endl;
+    
+    return 0;
+}
